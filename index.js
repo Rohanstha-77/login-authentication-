8af6ba0111fecf788476744cddc7805836d49ecd
Rohan-Stha <114400824+Rohanstha-77@users.noreply.github.com>
2023-07-04 18:45:29 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 function login() {
-    // Get the input values
     var id = document.getElementById("id").value;
     var password = document.getElementById("password").value;
-  
-    // Perform basic validation
     if (id === "" || password === "") {
       alert("Please enter both ID and password.");
       return;
     }
-  
-    // Perform login authentication
     if (id === "admin" && password === "22061003") {
     //   alert("Login successful!");
-      // You can redirect to another page or perform additional actions here
-      window.location.href="template.html"
+      window.location.href="template.html" //replace your with templete.html
     } else {
       alert("Invalid ID or password. Please try again.");
     }
